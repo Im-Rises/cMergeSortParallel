1eb3280b37643f3a35067231ffbb5f662570b5d2
Im-Rises <quentin-morel88@hotmail.com>
2022-11-03 17:08:23 -0400

    Added new lib install for macOs Cmake build

--- OpenMpVersion/d2omp.c
+++ OpenMpVersion/d2omp.c
@@ -2,22 +2,6 @@
 #include <omp.h>
 
 int main() {
-    // #pragma omp for
-    //     for (int i=0;i <1000;i++)
-    //     {
-    //         printf("Thread: %d - Value: %d\n", omp_get_thread_num(),i);
-    //     }
-
-#pragma omp parallel
-    {
-#pragma omp task
-        printf("1 %d: ", omp_get_thread_num());
-#pragma omp task
-        printf("2 %d: ", omp_get_thread_num());
-#pragma omp task
-        printf("3 %d: ", omp_get_thread_num());
-#pragma omp task
-        printf("4 %d: ", omp_get_thread_num());
-    }
+    printf("%d", omp_get_max_threads());
     return 0;
 }
