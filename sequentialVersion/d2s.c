4ff8f2f75e286bf36fda2f0fe67c32cf44710522
Im-Rises <quentin-morel88@hotmail.com>
2022-11-04 14:02:08 -0400

    wip

--- sequentialVersion/d2s.c
+++ sequentialVersion/d2s.c
@@ -1,57 +1,73 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define ARRAY_SIZE 3
 
 void printArray(int* array, int size) {
     for (int i = 0; i < size; i++)
     {
         printf("%d ", array[i]);
     }
+    printf("\n");
 }
 
-void merge(int U[], int V[], int T[], int n) {
-    int i = 0;
-    int j = 0;
-    for (int k = 0; k < n; k++)
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int binarySearch(int* arr, int x, int l, int h) {
+    if (l > h)
+        return -1;
+    else
     {
-        if (U[i] < V[j])
-        {
-            T[k] = U[i++];
-        }
+        int mid = (l + h) / 2;
+        if (arr[mid] == x)
+            return mid;
+        else if (arr[mid] > x)
+            return binarySearch(arr, x, l, mid - 1);
         else
-        {
-            T[k] = V[j++];
-        }
+            return binarySearch(arr, x, mid + 1, h);
     }
 }
 
-void mergeSort(int T[], int n, int U[], int V[]) {
-    if (n <= 1)
+void merge(int T[], int p1, int r1, int p2, int r2, int A[], int p3) {
+    int n1 = r1 - p1 + 1;
+    int n2 = r2 - p2 + 1;
+    if (n1 < n2)
     {
+        swap(&n1, &n2);
+        swap(&p1, &p2);
+        swap(&r1, &r2);
+    }
+    else
+    {
+        int q1 = (p1 + r1) / 2;
+        int q2 = binarySearch(T, T[q1], p2, r2);
+        int q3 = p3 + (q1 - p1) + (q2 - p2);
+        A[q3] = T[q1];
+        merge(T, p1, q1 - 1, p2, q2 - 1, A, p3);
+        merge(T, q1 + 1, r1, q2, r2, A, q3 + 1);
+    }
+}
+
+void mergeSort(int A[], int p, int r) {
+    if (p >= r)
         return;
-    }
-
-    for (int i = 0; i < n / 2; i++)
-    {
-        U[i] = T[i];
-        V[i] = T[i + n / 2];
-    }
-
-    //    mergeSort(U, n / 2, U, V);
-    //    mergeSort(V, n / 2, U, V);
-    //    merge(U, V, T, n);
+    int q = (p + r) / 2;
+    mergeSort(A, p, q);
+    mergeSort(A, q + 1, r);
+    merge(A, p, q, q + 1, r, A, p);
 }
-
-#define ARRAY_SIZE 10
 
 int main() {
-    int array[ARRAY_SIZE] = { 10, 2, 3, 40, 5, 6, 70, 8, 9, 10 };
-    int* U = malloc(ARRAY_SIZE * sizeof(int));
-    int* V = malloc(ARRAY_SIZE * sizeof(int));
+    int array[ARRAY_SIZE] = { 10, 2, 3 };
 
-    mergeSort(array, ARRAY_SIZE, U, V);
+    printArray(array, ARRAY_SIZE);
 
-    free(U);
-    free(V);
+    mergeSort(array, 0, ARRAY_SIZE);
 
     printArray(array, ARRAY_SIZE);
 
---
+++ sequentialVersion/d2s_old.c
@@ -0,0 +1,72 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ARRAY_SIZE 3
+
+void printArray(int* array, int size) {
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void merge(int U[], int V[], int T[], int n) {
+    int i = 0;
+    int j = 0;
+    U[n / 2] = INT_MAX;
+    V[n / 2] = INT_MAX;
+    for (int k = 0; k < n; k++)
+    {
+        if (U[i] < V[j])
+        {
+            T[k] = U[i++];
+        }
+        else
+        {
+            T[k] = V[j++];
+        }
+    }
+}
+
+void mergeSort(int T[], int n) {
+    if (n <= 1)
+    {
+        return;
+    }
+
+    int* U = malloc(((n / 2) + 1) * sizeof(int));
+    int* V = malloc(((n / 2) + 1) * sizeof(int));
+    if (U == NULL || V == NULL)
+    {
+        printf("Memory allocation error!");
+        exit(1);
+    }
+
+
+    for (int i = 0; i < n / 2; i++)
+    {
+        U[i] = T[i];
+        V[i] = T[i + n / 2];
+    }
+
+    mergeSort(U, n / 2);
+    mergeSort(V, n / 2);
+    merge(U, V, T, n);
+
+    free(U);
+    free(V);
+}
+
+int main() {
+    int array[ARRAY_SIZE] = { 10, 2, 3 };
+
+    printArray(array, ARRAY_SIZE);
+
+    mergeSort(array, ARRAY_SIZE);
+
+    printArray(array, ARRAY_SIZE);
+
+    return 0;
+}
