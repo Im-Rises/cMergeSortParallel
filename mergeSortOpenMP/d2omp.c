6a8afe16febb83b05f2e48bce763c98093964d3d
Im-Rises <Quentin-morel88@hotmail.com>
2022-11-07 15:46:12 -0500

    wip

--- mergeSortOpenMP/d2omp.c
+++ mergeSortOpenMP/d2omp.c
@@ -6,6 +6,9 @@
 
 #define MAX_THREADS 4
 
+#define MAX_NUMBER_PRINT 100
+#define COMPLETE_NUMBER_PRINT_THRESHOLD 1000
+
 void mergeSortParallel(int A[], int p, int r);
 void mergeSort(int A[], int p, int r);
 void merge(int A[], int p, int q, int r);
@@ -22,17 +25,16 @@ void printArray(const int* array, int begin, int size);
 int main() {
     /*
      * Error list:
-     * 1 - Wrong number of arguments
-     * 2 - Error opening file
-     * 3 - Error reading line from file
-     * 4 - Error allocating memory
+     * 1 - Error reading line from file
+     * 2 - Error allocating memory
      */
+
     printf("-----Merge Sort Parallel using OpenMP-----\n\n");
 
     /* Read size of array from stream */
     int arraySize = 0;
     scanf("%d", &arraySize);
-    printf("Array size: %d\n", arraySize);
+    printf("- Array size: %d\n", arraySize);
 
     /* Create array */
     int* inputArray = allocateMemory(arraySize * sizeof(int));
@@ -43,8 +45,8 @@ int main() {
     {
         if (scanf("%d", &inputArray[i]) != 1)
         {
-            printf("Error reading line from stream");
-            return 3;
+            fprintf(stderr, "Error reading line from stream");
+            return 1;
         }
     }
 
@@ -76,9 +78,9 @@ void mergeSortParallel(int A[], int p, int r) {
     {
         int q = (p + r) / 2;
 
-#pragma omp task shared(A)
+#pragma omp task shared(A) private(p, r)
         mergeSort(A, p, q);
-#pragma omp task shared(A)
+#pragma omp task shared(A) private(p, r)
         mergeSort(A, q + 1, r);
 #pragma omp taskwait
         merge(A, p, q, r);
@@ -96,13 +98,13 @@ void mergeSort(int A[], int p, int r) {
 }
 
 void merge(int A[], int p, int q, int r) {
+    int i, j, k;
+
     int n1 = q - p + 1;
     int n2 = r - q;
 
     int L[n1 + 1];
     int R[n2 + 1];
-
-    int i, j, k;
 
     for (i = 0; i < n1; i++)
         L[i] = A[p + i];
@@ -133,21 +135,21 @@ void* allocateMemory(size_t size) {
     void* memory = malloc(size);
     if (memory == NULL)
     {
-        printf("Error allocating memory");
-        exit(4);
+        fprintf(stderr, "Error allocating memory");
+        exit(2);
     }
     return memory;
 }
 
 void printArraySummary(int* array, int arraySize) {
     printf("Array sorted:\n");
-    if (arraySize > 1000)
+    if (arraySize > COMPLETE_NUMBER_PRINT_THRESHOLD)
     {
         printf("Array too big to print\n");
-        printf("- First 100 values: \n");
+        printf("- First %d values: \n", MAX_NUMBER_PRINT);
         printArray(array, 0, 100);
         printf("...\n");
-        printf("- Last 100 values: \n");
+        printf("- Last %d values: \n", MAX_NUMBER_PRINT);
         printArray(array, arraySize - 100, arraySize);
     }
     else
@@ -162,78 +164,3 @@ void printArray(const int* array, const int begin, const int size) {
         printf("%d\n", array[i]);
     printf("\n");
 }
-
-
-/*--------------OTHER MERGE SORT FUNCTIONS--------------*/
-/*
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int binarySearch(int x, int T[], int p, int r) {
-    int low = p;
-    int high = p > r + 1 ? p : r + 1;
-    while (low < high)
-    {
-        int mid = (low + high) / 2;
-        if (x <= T[mid])
-            high = mid;
-        else
-            low = mid + 1;
-    }
-    return high;
-}
-
-void parallelMerge(int T[], int p1, int r1, int p2, int r2, int A[], int p3) {
-    int n1 = r1 - p1 + 1;
-    int n2 = r2 - p2 + 1;
-    if (n1 < n2)
-    {
-        swap(&p1, &p2);
-        swap(&r1, &r2);
-        swap(&n1, &n2);
-    }
-    if (n1 == 0)
-        return;
-    else
-    {
-        int q1 = (p1 + r1) / 2;
-        int q2 = binarySearch(T[q1], T, p2, r2);
-        int q3 = p3 + (q1 - p1) + (q2 - p2);
-        A[q3] = T[q1];
-        */
-/*#pragma omp parallel sections
-      {
-#pragma omp section*/
-/*
-parallelMerge(T, p1, q1 - 1, p2, q2 - 1, A, p3);
-*/
-/*#pragma omp section*/ /*
-           parallelMerge(T, q1 + 1, r1, q2, r2, A, q3 + 1);
-       */
-/*}*/                   /*
-             }
-         }
-
-         void parallelMergeSort(int A[], int p, int r, int B[], int s) {
-             int n = r - p + 1;
-             if (n == 1)
-                 B[s] = A[p];
-             else
-             {
-                 int T[n];
-
-                 int q = (p + r) / 2;
-                 int q2 = q - p + 1;
-         #pragma omp parallel sections
-                 {
-         #pragma omp section
-                     parallelMergeSort(A, p, q, T, 0);
-         #pragma omp section
-                     parallelMergeSort(A, q + 1, r, T, q2);
-                 }
-                 parallelMerge(T, 0, q2 - 1, q2, n - 1, B, s);
-             }
-         }*/
